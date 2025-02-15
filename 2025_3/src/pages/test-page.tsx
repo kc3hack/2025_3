95eb901f46e888b0e7163cc6eafe05af301761fa
kanekos619 <kasouya0619@outlook.jp>
2025-02-15 16:29:35 +0900

    functionTest

--- 2025_3/src/pages/test-page.tsx
+++ 2025_3/src/pages/test-page.tsx
@@ -1,6 +1,7 @@
 import "../css_designs/test-page.css";
+import { testfnc1 } from "../api/testfnc";
 const TestPage = () => {
-    const buttonTest = () => {  console.log("Button Clicked");  };
+    // const buttonTest = () => {  console.log("Button Clicked");  };
   return (
     <>
   
@@ -8,7 +9,7 @@ const TestPage = () => {
 <div className="testDiv Case3">div3
     <div className="testDiv Case4">div4</div>
     <div className="testDiv Case5">div5</div>
-    <button className="testButton" onClick={buttonTest}>Click Me</button>
+    <button className="testButton" onClick={testfnc1}>Click Me</button>
    </div> 
      <div className="testDiv Case1">div1</div>
     <div className="testDiv Case2">div2</div>
