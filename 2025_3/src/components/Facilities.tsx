6ce3e51a87426e8326ab9962d40dee9539a44cb0
daidai411411 <daidai411411@gmail.com>
2025-02-18 20:37:49 +0900

    施設ウィンドウを実装(Material UIを使用)

--- 2025_3/src/components/Facilities.tsx
+++ 2025_3/src/components/Facilities.tsx
@@ -1,6 +1,94 @@
-function Facilities() {
+import {
+  Box,
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+} from "@mui/material";
+import "../css_designs/Facilities.css";
+
+type Facility = {
+  name: string;
+  img_path: string;
+  efficiency: number;
+  cost: number;
+  magnification: number;
+  isLocked: boolean;
+};
+
+type Props = {
+  facilities: Facility[];
+  money: number;
+  setMoney: React.Dispatch<React.SetStateAction<number>>;
+  facilitylevels: number[];
+  setLevels: React.Dispatch<React.SetStateAction<number[]>>;
+};
+
+function Facilities({
+  facilities,
+  money,
+  setMoney,
+  facilitylevels: levels,
+  setLevels,
+}: Props) {
+  function onFacilityClick(idx: number) {
+    setMoney(
+      money -
+        facilities[idx].cost * facilities[idx].magnification ** levels[idx]
+    );
+    const newLevels = levels.slice();
+    newLevels[idx]++;
+    setLevels(newLevels);
+  }
+
+  const facilityItems = facilities.map((facility, idx) => {
+    const cost = facility.cost * facility.magnification ** levels[idx];
+    return (
+      <ListItem className="facility-item" key={idx}>
+        <ListItemButton
+          className="facility-button"
+          onClick={() => onFacilityClick(idx)}
+          disabled={facility.isLocked || money < cost}
+        >
+          <div className="facility-info">
+            <ListItemAvatar
+              sx={{
+                filter: facility.isLocked ? "brightness(0%) blur(3px)" : "none",
+              }}
+            >
+              <img
+                className="facility-avatar"
+                src={facility.img_path}
+                alt="facility image"
+              />
+            </ListItemAvatar>
+            {facility.isLocked ? (
+              "？？？"
+            ) : (
+              <>
+                {facility.name}
+                <br />
+                {"¥" + cost.toLocaleString()}
+              </>
+            )}
+          </div>
+          {!facility.isLocked && (
+            <div className="facility-level">Lv.{levels[idx]}</div>
+          )}
+        </ListItemButton>
+      </ListItem>
+    );
+  });
+
   return (
-    <></>
+    <Box className="facilities-box">
+      <h1>施設</h1>
+      <Divider className="facilities-divider" />
+      <Box className="facilities-list">
+        <List>{facilityItems}</List>
+      </Box>
+    </Box>
   );
 }
 
--- 2025_3/src/pages/test-page.tsx
+++ 2025_3/src/pages/test-page.tsx
@@ -1,19 +1,69 @@
+// Facilities.tsx の動作を確認するためのテストページ
+
+import Facilities from "../components/Facilities";
 import "../css_designs/test-page.css";
-const TestPage = () => {
-    const buttonTest = () => {  console.log("Button Clicked");  };
-  return (
-    <>
-  
+import { useState } from "react";
 
-<div className="testDiv Case3">div3
-    <div className="testDiv Case4">div4</div>
-    <div className="testDiv Case5">div5</div>
-    <button className="testButton" onClick={buttonTest}>Click Me</button>
-   </div> 
-     <div className="testDiv Case1">div1</div>
-    <div className="testDiv Case2">div2</div>
-
-    </>
-  );
+type Facility = {
+  name: string;
+  img_path: string;
+  efficiency: number;
+  cost: number;
+  magnification: number;
+  isLocked: boolean;
 };
+
+function TestPage() {
+  const facilities: Facility[] = [];
+  for (let i = 0; i < 10; i++) {
+    facilities.push({
+      name: "施設" + i,
+      img_path: "/landmark_oosakajou.png",
+      efficiency: 1.0,
+      cost: 1000000 * (i + 1),
+      magnification: 2.0,
+      isLocked: true,
+    });
+  }
+  facilities[0].isLocked = facilities[1].isLocked = false;
+
+  const [money, setMoney] = useState(10_000_000);
+
+  const [levels, setLevels] = useState<number[]>(
+    Array(facilities.length).fill(0)
+  );
+
+  return (
+    <div className="test-page">
+      {"money: " + money}
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <h1>Test Page</h1>
+      <div className="facilities">
+        <Facilities
+          facilities={facilities}
+          money={money}
+          setMoney={setMoney}
+          facilitylevels={levels}
+          setLevels={setLevels}
+        />
+      </div>
+    </div>
+  );
+}
+
 export default TestPage;
