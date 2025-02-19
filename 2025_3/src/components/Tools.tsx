4d770277c5187862bfbc3783ce1213247dac3040
Aoki-3DPr <s2410001.aoki.cgp@gmail.com>
2025-02-19 20:57:01 +0900

    Tools追加

--- 2025_3/src/components/Tools.tsx
+++ 2025_3/src/components/Tools.tsx
@@ -1,9 +1,36 @@
-function Tools() {
-  return (
-    <div>
+import "../components/Tools.css";
+import { useState } from "react";
+import tipA from "../assets/a.png";
+import tipB from "../assets/b.png";
+import tipC from "../assets/c.png";
 
-    </div>
+const Tools = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [tipA, tipB, tipC];
+
+  const buttonTest2 = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  return (
+    <>
+      <div className="testDiv Case2">
+        div2
+        <div className="testDiv11 ">
+          ➡
+        </div>
+        <div className="testDiv Case6">
+          <img src={images[currentImageIndex]} alt="画像1" />
+        </div>
+        <div className="testDiv Case7">
+          <img src={images[(currentImageIndex + 1) % images.length]} alt="画像2" />
+        </div>
+        <button className="testButton" onClick={buttonTest2}>
+          Change Image
+        </button>
+      </div>
+    </>
   );
-}
+};
 
 export default Tools;
