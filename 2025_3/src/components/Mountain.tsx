7aba4aef195117bb58759020f1545c137a53b4d6
FUJITAKONNRO <rakkukusu@gmail.com>
2025-02-20 09:59:57 +0900

    山をでかくしました

--- 2025_3/src/components/Mountain.tsx
+++ 2025_3/src/components/Mountain.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Mountain.css";
 function Mountain({ onButtonClick }) {
   return (
-    <div >
+    <div className="mountain-container">
       <button className="button" onClick={onButtonClick}>Mountain</button>
     </div>
   );
