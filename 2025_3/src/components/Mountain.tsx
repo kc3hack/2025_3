e3e818e694c1b70bd4ae67613423a34ff19682e5
Dango <aujgadm3521@gmail.com>
2025-02-23 15:12:56 +0900

    山育てよう

--- 2025_3/src/components/Mountain.tsx
+++ 2025_3/src/components/Mountain.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "../css_designs/Mountain.css";
+import { useElevation } from "../api/context/get_edit";
 function Mountain({ onButtonClick }) {
+  const elevation = useElevation();
   return (
-    <div className="mountain-container">
+    <div className="mountain-container" style={{ height: `${10+(elevation)*0.088}%`, width: `${50+(elevation)*0.02}%` }}>
       <button className="button" onClick={onButtonClick}>Mountain</button>
     </div>
   );
